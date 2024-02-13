3c3cf3bc189c7969989967a5710a5e97814fd32f
Filippo <corti.filippo03@gmail.com>
2024-02-13 19:41:52 +0100

    Sliders

--- MioProgetto/js/slider.js
+++ MioProgetto/js/slider.js
@@ -1,47 +1,47 @@
-const CARD_SIZE = 15 + 1; //rem
-let sliders = document.querySelectorAll(".slider");
-
-var current_cards = [];
-var cards_numbers = [];
-var sliding_windows = [];
-
-initSliders();
-
-function initSliders() {
-    let i = 0;
-    for (let slider of sliders) {
-        sliding_windows.push(slider.querySelector(".sliding-window"));
-        var cards_number = slider.querySelectorAll(".sliding-window > .card").length - Math.ceil(slider.querySelector(".sliding-container").clientWidth / (16 * CARD_SIZE)) + 1; //16px = 1rem
-        console.log(cards_number);
-        cards_numbers.push(cards_number);
-        var current_card = 0;
-        current_cards.push(current_card);
-        var button_next = slider.querySelector("button.next");
-        var button_previous = slider.querySelector("button.previous");
-        button_next.setAttribute("data-sliders-index", i);
-        button_previous.setAttribute("data-sliders-index", i);
-        console.log(button_next);
-        i++;
-    }
-
-}
-
 
 function next(button) {
-    let i = button.getAttribute("data-sliders-index"); current_cards[i]++;
-    if (current_cards[i] >= cards_numbers[i]) {
+    //Find components
+    let slider_id = button.getAttribute("data-slider-for");
+    let target_slider = document.getElementById(slider_id);
+    let current_card = target_slider.getAttribute("data-slider-current");
+    let sliding_window = target_slider.querySelector(".sliding-window");
+    let cards = sliding_window.querySelectorAll("[data-slider-scope='card']");
+    let card_width = cards[0].clientWidth + 16; //16px is the gap
+
+    //Update current card
+    current_card++;
+    if (current_card >= cards.length - 1 - sliding_window.clientWidth / card_width) {
         button.disabled = true;
-    } else {
-        sliding_windows[i].style.transform = "translateX(-" + current_cards[i] * CARD_SIZE + "rem)";
     }
+    target_slider.setAttribute("data-slider-current", current_card);
+
+    //Enable other button
+    let other_button = target_slider.querySelector("button.previous");
+    other_button.disabled = false;
+
+    //Slide
+    sliding_window.style.transform = "translateX(-" + current_card * card_width + "px)";
 }
 
 function previous(button) {
-    let i = button.getAttribute("data-sliders-index");
-    current_cards[i]--;
-    if (current_cards[i] < 0) {
+    //Find components
+    let slider_id = button.getAttribute("data-slider-for");
+    let target_slider = document.getElementById(slider_id);
+    let current_card = target_slider.getAttribute("data-slider-current");
+    let sliding_window = target_slider.querySelector(".sliding-window");
+    let cards = sliding_window.querySelectorAll("[data-slider-scope='card']");
+    let card_width = cards[0].clientWidth + 16; //16px is the gap
+
+    //Update current card
+    current_card--;
+    if (current_card == 0) {
         button.disabled = true;
-    } else {
-        sliding_windows[i].style.transform = "translateX(-" + current_cards[i] * CARD_SIZE + "rem)";
     }
+    target_slider.setAttribute("data-slider-current", current_card);
+
+    //Enable other button
+    let other_button = target_slider.querySelector("button.next");
+    other_button.disabled = false;
+
+    sliding_window.style.transform = "translateX(-" + current_card * card_width + "px)";
 }
