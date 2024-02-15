f655214bc5db5dfba69eeb7fa3065234bb503065
SaraBuzzi <sarabuzzi127@gmail.com>
2024-02-16 00:18:58 +0100

    prove di API

--- MioProgetto/js/api.js
+++ MioProgetto/js/api.js
@@ -23,3 +23,54 @@ async function getById(id) {
 }
 
 
+async function getCategories() {
+    let categories;
+    await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(response => response.json())
+        .then(response => {
+            //Elaborazione
+            categories = response;
+        })
+        .catch(err => console.error(err));
+    return categories;
+}
+
+function randomCategories() {
+    let num;
+    num = Math.floor(Math.random() * 10) + 4;
+    return num;
+}
+
+async function getAreas() {
+    let areas;
+    await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+        .then(response => response.json())
+        .then(response => {
+            //Elaborazione
+            areas = response;
+        })
+        .catch(err => console.error(err));
+    return areas;
+}
+
+function randomAreas() {
+    let num;
+    num = Math.floor(Math.random() * 10) + 18;
+    return num;
+}
+
+async function getByArea(areaName) {
+    let areaRecipes;
+    await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=" + areaName)
+        .then(response => response.json())
+        .then(response => {
+            //Elaborazione
+            areaRecipes = response;
+        })
+        .catch(err => console.error(err));
+    return areaRecipes;
+}
+
+
+
+
